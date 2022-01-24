2d27a0533f78d7ce2a1b978ddd3de1c952040219
Michel Silva <michel.barbosa@hotmail.com>
2022-01-24 15:15:47 -0300

    confirmar em 'main'

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function checkChar(e) {
       return true;
   }
 }
+
 function codificar(texto) {
     texto = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
     return texto;
